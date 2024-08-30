386dee1f7da945cb37e8d225a39f6b9da008fd9e
Gilles ZAOUI <gizaoui@gmail.com>
2024-08-30 15:19:39 +0200

    .

--- html/simple-project/src/Entity/Category.php
+++ html/simple-project/src/Entity/Category.php
@@ -4,77 +4,83 @@ namespace App\Entity;
 
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Validator\BanWord;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
-class Category
-{
+#[UniqueEntity('name')]
+#[UniqueEntity('slug')]
+class Category {
+    
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
+    
     #[ORM\Column(length: 70)]
-    private ?string $name = null;
-
+    #[Assert\Length(min: 5)]
+    #[BanWord(groups: ['Extra'])]
+    private string $name = '';
+    
     #[ORM\Column(length: 70)]
-    private ?string $slug = null;
-
+    #[Assert\Length(min: 5)]
+    #[Assert\Regex('/^[a-z0-9]+(?:-[a-z0-9]+)*$/', 'Invalid slug', groups: ['Extra'])]
+    private string $slug = '';
+    
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
-
+    
     #[ORM\Column]
     private ?\DateTimeImmutable $updateAt = null;
-
+    
     public function getId(): ?int
     {
         return $this->id;
     }
-
-    public function getName(): ?string
+    
+    public function getName(): string
     {
         return $this->name;
     }
-
+    
     public function setName(string $name): static
     {
         $this->name = $name;
-
         return $this;
     }
-
-    public function getSlug(): ?string
+    
+    public function getSlug(): string
     {
         return $this->slug;
     }
-
+    
     public function setSlug(string $slug): static
     {
         $this->slug = $slug;
-
         return $this;
     }
-
+    
     public function getCreatedAt(): ?\DateTimeImmutable
     {
         return $this->createdAt;
     }
-
+    
     public function setCreatedAt(\DateTimeImmutable $createdAt): static
     {
         $this->createdAt = $createdAt;
-
         return $this;
     }
-
+    
     public function getUpdateAt(): ?\DateTimeImmutable
     {
         return $this->updateAt;
     }
-
+    
     public function setUpdateAt(\DateTimeImmutable $updateAt): static
     {
         $this->updateAt = $updateAt;
-
+        
         return $this;
     }
 }
